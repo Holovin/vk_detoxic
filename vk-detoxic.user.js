c398739b120eb03c2aa53b205bc11c3b9c501718
Alexander Holovin <dvjmixxxer@gmail.com>
2021-03-15 08:04:02 +0300

    New version via mocking httpreq

--- vk-detoxic.user.js
+++ vk-detoxic.user.js
@@ -1,159 +1,77 @@
 // ==UserScript==
-// @name         VK Detoxic
-// @namespace    https://holov.in/vkdetoxic
-// @version      0.0.4b
-// @description  Hey hey
-// @author       Alexander Holovin
-// @match        https://vk.com/*
+// @name         VK Detox 2
+// @namespace    https://holov.in/ujs/VKD2
+// @version      1.99.114
+// @description  VK Detox 2
+// @author       Alex Holovin
+// @match        https://*.vk.com/*
+// @copyright    Alex Holovin, based on https://openuserjs.org/scripts/flyink13/Web_Im_show_deleted_messages
 // @license      MIT
-// @grant        none
-// @noframes
 // ==/UserScript==
 
-window.addEventListener('load', detox, false);
+/* global geByClass1 */
 
-function detox() {
-    'use strict';
-    console.warn('DetoxLoaded v4');
+function showDeletedMessages() {
+  const ID = 572200511;
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  console.log('[VK Detox] Running: 1.99.114');
 
-    const dialogNames = ['ONE', 'TWO'];
-    const banUserId = '000';
-    const banUserName = 'NAME';
-    const banUserLink = '/userlink';
+  XMLHttpRequest.prototype.send = (function buildFakeSend(sendOrg) {
+    return function fakeSend(...sendArgs) {
+      const self = this;
+      self.onreadystatechange = (function buildFakeOnChang(changeOrg) {
+        if (!changeOrg) { return; }
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        return function fakeOnReadyStateChange(...changeArgs) {
+          const isLongPoll = (/^https:\/\/.?im(v4)?\.vk\.com\/.?im\d+/.test(self.responseURL));
+          const isReady = (self.readyState === 4);
 
-    const pushStateFunction = history.pushState;
-    let lastMessageId = -1;
-    let dialogObserver = null;
+          if (isLongPoll && isReady && self.responseText) {
+            try {
+              const newJson = JSON.parse(self.responseText);
 
-    detectChanges();
+              newJson.updates.forEach((update) => {
+                // console.log(update); // debug
 
-    history.pushState = function() {
-        pushStateFunction.apply(history, arguments);
-        setTimeout(detectChanges, 750);
+                const isTyping = (update[0] === 63 && update[2] === ID);
+                if (isTyping) {
+                  // TODO
+                }
+
+                const isMessage = (update[0] === 4 && update[7].from == ID);
+                if (isMessage) {
+                  setTimeout(() => {
+                    const el = geByClass1('_im_mess_' + update[1]).parentElement.parentElement.parentElement;
+                    el.style.filter = 'blur(8px) opacity(0.5)';
+                    el.parentElement.style.overflow = 'hidden';
+                  });
+                }
+
+                const isReply = (update[0] === 4 && update[7].marked_users[0].find(subArr => Array.isArray(subArr) && subArr.includes(ID)));
+                if (isReply) {
+                  setTimeout(() => {
+                    const el = geByClass1('_im_msg_reply' + update[1] + ' > div')
+                    el.style.filter = 'blur(8px) opacity(0.5)';
+                    el.parentElement.style.overflow = 'hidden';
+                  });
+                }
+              });
+
+            } catch(e) {}
+          }
+
+          return changeOrg.apply(self, changeArgs);
+        };
+      })(self.onreadystatechange);
+
+      return sendOrg.apply(self, sendArgs);
     };
-
-    function detectChanges() {
-        try {
-            const dialogTarget = document.querySelector('div.im-page-history-w');
-
-            if (!dialogTarget) {
-                console.debug('[VA] Нет диалогов, пропуск');
-                return;
-            }
-
-            const dialogElement = dialogTarget.querySelector('span.im-page--title-main');
-            const currentDialogName = dialogElement.title;
-            const isDialogOpen = !!dialogElement.offsetParent;
-            console.debug(`[VA] Диалог: ${currentDialogName} ${isDialogOpen ? 'открыт' : 'закрыт'}`);
-
-            if (dialogObserver) {
-                console.warn('[VA] Детокс выкл.');
-                dialogObserver.disconnect();
-                dialogObserver = null;
-            }
-
-            if (isDialogOpen && dialogNames.includes(currentDialogName)) {
-                console.warn('[VA] Детокс вкл.');
-
-                hidePreloadedMessages();
-                startChatObserver(dialogTarget);
-            }
-
-        } catch (error) {
-            console.warn(`[VA] ${error}\n${error.stack}`);
-        }
-    }
-
-    function hidePreloadedMessages() {
-        const messages = [...document.querySelectorAll('li.im-mess')].slice(-20);
-        messages.forEach(item => processMessage(item));
-    }
-
-    function startChatObserver(dialogTarget) {
-        const config = { attributes: false, childList: true, characterData: true, subtree: true };
-        dialogObserver = new MutationObserver(mutations => {
-            mutations.forEach(mutation => {
-                if (mutation.type !== 'childList' || mutation.addedNodes.length === 0) {
-                    return;
-                }
-
-                const messageId = mutation.target.dataset.ts;
-
-                if (!messageId || lastMessageId > messageId) {
-                    return;
-                }
-
-                lastMessageId = messageId;
-
-                if (!mutation.target.classList.contains('im-mess') || mutation.target.classList.contains('im-mess_out')) {
-                    return;
-                }
-
-                processMessage(mutation.target);
-            });
-        });
-
-        dialogObserver.observe(dialogTarget, config);
-    }
-
-    function processMessage(element) {
-        try {
-            const messageElement = element.parentElement.parentElement.parentElement;
-            const userId = messageElement.dataset.peer;
-
-            if (userId === banUserId) {
-                console.warn('[VA] fixed ok: by user id');
-                hideElement(messageElement);
-                return;
-            }
-
-            const answerBlock = element.getElementsByClassName('im-replied--author-link _im_replied_author_link');
-            if (answerBlock.length && answerBlock[0].text === banUserName) {
-                hideStacked(element, 'reply');
-                return;
-            }
-
-            const forwardBlock = element.getElementsByClassName('media_desc im-mess--inline-fwd');
-            if (forwardBlock.length) {
-                const userUrl = forwardBlock[0].getElementsByClassName('im_grid');
-
-                if (userUrl.length && userUrl[0].href.indexOf(banUserLink) !== -1) {
-                    hideStacked(element, 'forward');
-                    return;
-                }
-            }
-
-            console.debug('[VA] skip');
-
-        } catch (error) {
-            console.warn(`[VA] ${error}\n${error.stack}`);
-        }
-    }
-
-    function hideElement(element) {
-        console.log('Hide: ', element);
-        element.style.display = 'none';
-    }
-
-    function fakeHideElement(element) {
-        console.log('FakeHide: ', element);
-        element.style.height = '1px';
-        element.style.padding = '22px 0 0 0';
-        element.style.opacity = '0';
-    }
-
-    function hideStacked(element, reason) {
-        if (element.nextElementSibling) {
-            console.warn(`[VA] fixed ok: by ${reason} (partial)`);
-            fakeHideElement(element);
-            return;
-        }
-
-        console.warn(`[VA] fixed ok: by ${reason} (full)`);
-        hideElement(element.parentElement.parentElement.parentElement);
-    }
+  })(XMLHttpRequest.prototype.send);
 }
+
+(function injectScript() {
+  const script = document.createElement('script');
+  const code = '(' + showDeletedMessages + ')();';
+  script.appendChild(document.createTextNode(code));
+  (document.body || document.head || document.documentElement).appendChild(script);
+})();
